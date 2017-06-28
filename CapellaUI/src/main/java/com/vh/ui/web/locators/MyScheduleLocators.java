75418712f1ccf2ccdcd83b1c2bf0dfb94f87bcd8
m7hacke <harvy.ackermans@villagehealth.com>
2017-06-28 12:43:13 -0500

    My Schedule
    My Schedule

---
+++ CapellaUI/src/main/java/com/vh/ui/web/locators/MyScheduleLocators.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.vh.ui.web.locators;
+
+/*
+ * @author Harvy Ackermans
+ * @date   March 28, 2017
+ * @class  MyPatientsLocators.java
+ */
+
+public class MyScheduleLocators
+{
+
+}
